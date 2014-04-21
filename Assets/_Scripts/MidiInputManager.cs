be26a65159901936f18e42b9c32865e473fe9788
Travis Dean <travis.j.dean@gmail.com>
2014-04-21 02:02:29 -0400

    Extensive updates. Added test environment using lux shaders. Began implementation of event type MVC architecture for input -> output control.

---
+++ Assets/_Scripts/MidiInputManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+
+public class MidiInputManager : MonoBehaviour, IInputManager
+{
+    public event EventHandler PianoKeyDown;
+
+    public void pianoKeyDown()
+    {
+        OnPianoKeyDown(new EventArgs());
+    }
+
+    void OnPianoKeyDown(EventArgs e)
+    {
+        if (PianoKeyDown != null)
+            PianoKeyDown(this, e);
+    }
+}
---
+++ Assets/_Scripts/UnityInputManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+public class UnityInputManager : MonoBehaviour, IInputManager
+{
+    public event EventHandler PianoKeyDown;
+
+    public void Update()
+    {
+        if (Input.GetAxisRaw("Fire2") > 0)
+        {
+            pianoKeyDown();
+            Input.ResetInputAxes();
+        }
+    }
+
+    // Probably can condense this with lambdas and such. Will be useful in future.
+    public void pianoKeyDown()
+    {
+        OnPianoKeyDown(new EventArgs());
+    }
+
+    void OnPianoKeyDown(EventArgs e)
+    {
+        if (PianoKeyDown != null)
+            PianoKeyDown(this, e);
+    }
+}
